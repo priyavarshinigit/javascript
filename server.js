d9ad925c12d0fcda520327d8ce6ddb618906deb8
priyavarshinigit <priya88varshini@gmail.com>
2024-09-28 12:51:08 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,4 +1,23 @@
-console.log("welcome to express js");
-console.log("welcome to second screnn");
-console.log("after adding nodemon library");
-console.log("adding again");
+const express=require('express')
+const MongoClient=require('mongoose')
+const dotEnv=require('dotenv')
+const bodyParser=require('body-parser')
+const EmployeeRoute=require('./routes/EmployeeRoutes')
+const app=express();
+app.use(bodyParser.json());
+dotEnv.config();
+
+const PORT=process.env.PORT || 7766;
+MongoClient.connect(process.env.MONGO_URI)
+.then(()=>{
+    console.log("MongoDB connection is success")
+})
+.catch((error)=>{
+    console.log(error);
+})
+
+app.use("/employees",EmployeeRoute);
+
+app.listen(PORT,()=>{
+    console.log(`My Server is running on ${PORT} number`)
+})
